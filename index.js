3e90140c36d805e751c6db0cce8fe98d81a64de1
syntaxmonkey2px <eopqrs@gmail.com>
2025-04-17 20:27:59 +0100

    fix: correct error response format for validation errors

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message 'i said 5 digits'})
+    return response.status(400).json({ error: error.message})
   }
 
   next(error)
